3d28d8b475c3764f723e9f43e4fb40ffc6b4cf97
Masih H. Derkani <m@derkani.org>
2023-01-26 12:46:11 +0000

    Implement `caskadht` service
    Implement `caskadht` as a service that exposes a HTTP API to lookup
    multihashes, compatible with IPNI `GET /multihash/<multihash>`
    specification and cascades lookups over the IPFS Kademlia DHT.
    
    Implement a CLI command to start up the service with options to
    configure libp2p host identity, listen address and log level.

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# caskadht
-:knot: Cascades lookups to the IPFS Kademlia DHT
+# :knot: caskadht
+
+`caskadht`, pronounced "Cascade-DHT", is a service that:
+* exposes an [IPNI-compatible](https://github.com/ipni/specs/blob/main/IPNI.md#get-multihashmultihash) `GET /multihash/<multihash>` endpoint over HTTP,
+* cascades lookup requests over the IPFS Kademlia DHT,
+* uses the accelerated DHT client when possible, and
+* steams the results back over `ndjson` whenever the request `Accept`s it or regular JSON otherwise.
+
+## Install
+
+To install `caskadht` CLI directly via Golang, run:
+
+```shell
+$ go install github.com/ipni/caskadht/cmd/caskadht@latest
+```
+
+## Usage
+
+```shell
+$ caskadht 
+Usage of caskadht:
+  -httpListenAddr string
+        The caskadht HTTP server listen address. (default "0.0.0.0:40080")
+  -libp2pIdentityPath string
+        The path to the marshalled libp2p host identity. If unspecified a random identity is generated.
+  -libp2pListenAddrs string
+        The comma separated libp2p host listen addrs. If unspecified the default listen addrs are used at ephemeral port.
+  -logLevel string
+        The logging level. Only applied if GOLOG_LOG_LEVEL environment variable is unset. (default "info")
+  -useAcceleratedDHT
+        Weather to use accelerated DHT client when possible. (default true)
+```
+
+### Run Server Locally
+
+To run the `caskadht` HTTP server locally, execute:
+
+```shell
+$ go run cmd/caskadht/main.go
+```
+The above command starts the HTTP API exposed on default listen address: `http://localhost:40080`.
+You can then start looking up multihashes, which would cascade onto the DHT. Example:
+```shell
+$ curl http://localhost:40080/multihash/QmcroxBV9PBPUg2LfeusC25x1C4mckSmQy6hD5rmuvugfj -v
+*   Trying 127.0.0.1:40080...
+* Connected to localhost (127.0.0.1) port 40080 (#0)
+> GET /multihash/QmcroxBV9PBPUg2LfeusC25x1C4mckSmQy6hD5rmuvugfj HTTP/1.1
+> Host: localhost:40080
+> User-Agent: curl/7.86.0
+> Accept: */*
+> 
+* Mark bundle as not supporting multiuse
+< HTTP/1.1 200 OK
+< Connection: Keep-Alive
+< Content-Type: application/x-ndjson
+< X-Content-Type-Options: nosniff
+< Date: Thu, 26 Jan 2023 12:17:02 GMT
+< Transfer-Encoding: chunked
+< 
+{"MultihashResults":[{"Multihash":"EiDXvX3xT1nGu2ZNdAM0rjm9g+/GAyirmnV5MfAVHoPLsg==","ProviderResults":[{"ContextID":"aXBmcy1kaHQtY2FzY2FkZQ==","Metadata":"gBI=","Provider":{"ID":"12D3KooWFRY4zb9Yvh7Pm5itdpVogtw2XDs68VgKwF1SkEy7eiEC","Addrs":[]}}]}]}
+
+```
+
+To shutdown the server, interrupt the terminal by pressing `Ctrl + C`
+
+## License
+
+[SPDX-License-Identifier: Apache-2.0 OR MIT](LICENSE.md)
---
+++ cmd/caskadht/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+
+	"github.com/ipfs/go-log/v2"
+	caskadht "github.com/ipni/caskadht"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	rcmgr "github.com/libp2p/go-libp2p/p2p/host/resource-manager"
+)
+
+var logger = log.Logger("caskadht/cmd")
+
+const (
+	libp2pUserAgent        = "ipni/caskadht"
+	libp2pMinConnsOutbound = 0xffff
+	libp2pMinBaseLimitFD   = 0xfff
+)
+
+func main() {
+	libp2pIdentityPath := flag.String("libp2pIdentityPath", "", "The path to the marshalled libp2p host identity. If unspecified a random identity is generated.")
+	libp2pListenAddrs := flag.String("libp2pListenAddrs", "", "The comma separated libp2p host listen addrs. If unspecified the default listen addrs are used at ephemeral port.")
+	httpListenAddr := flag.String("httpListenAddr", "0.0.0.0:40080", "The caskadht HTTP server listen address.")
+	useAcceleratedDHT := flag.Bool("useAcceleratedDHT", true, "Weather to use accelerated DHT client when possible.")
+	logLevel := flag.String("logLevel", "info", "The logging level. Only applied if GOLOG_LOG_LEVEL environment variable is unset.")
+	flag.Parse()
+
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
+		_ = log.SetLogLevel("*", *logLevel)
+	}
+
+	hOpts := []libp2p.Option{
+		libp2p.UserAgent(libp2pUserAgent),
+	}
+	if *libp2pIdentityPath != "" {
+		p := filepath.Clean(*libp2pIdentityPath)
+		logger := logger.With("path", p)
+		logger.Info("Unmarshalling libp2p host identity")
+		mid, err := os.ReadFile(p)
+		if err != nil {
+			logger.Fatalw("Failed to read libp2p host identity file", "err", err)
+		}
+		id, err := crypto.UnmarshalPrivateKey(mid)
+		if err != nil {
+			logger.Fatalw("Failed to unmarshal libp2p host identity file", "err", err)
+		}
+		hOpts = append(hOpts, libp2p.Identity(id))
+	}
+	if *libp2pListenAddrs != "" {
+		hOpts = append(hOpts, libp2p.ListenAddrStrings(strings.Split(*libp2pListenAddrs, ",")...))
+	}
+	if *useAcceleratedDHT {
+		// Adjust outbound connections and base limit FD to allow the accelerated DHT client to
+		// (re)load its routing table. Because, currently the client does not gracefully handle
+		// Resource Manager throttling.
+		//
+		// Note, a high outbound connection limit is considered less of a DoS risk than a high
+		// inbound connection limit. Further, due to the accelerated DHT client's behaviour, we only
+		// need higher connection limit; not more streams.
+		//
+		// Borrowed from:
+		//  - https://github.com/ipfs-shipyard/someguy/blob/84d56168e2dd6aae00e378a3ce8445de2c2f880f/server.go#L72
+		// TODO: Revisit limit increases once the the accelerated DHT client tolerates throttling.
+		defaultLimits := rcmgr.DefaultLimits
+		libp2p.SetDefaultServiceLimits(&defaultLimits)
+		if defaultLimits.SystemBaseLimit.ConnsOutbound < libp2pMinConnsOutbound {
+			defaultLimits.SystemBaseLimit.ConnsOutbound = libp2pMinConnsOutbound
+			if defaultLimits.SystemBaseLimit.Conns < defaultLimits.SystemBaseLimit.ConnsOutbound {
+				defaultLimits.SystemBaseLimit.Conns = defaultLimits.SystemBaseLimit.ConnsOutbound
+			}
+		}
+		if defaultLimits.SystemBaseLimit.FD < libp2pMinBaseLimitFD {
+			defaultLimits.SystemBaseLimit.FD = libp2pMinBaseLimitFD
+		}
+
+		rm, err := rcmgr.NewResourceManager(rcmgr.NewFixedLimiter(defaultLimits.AutoScale()))
+		if err != nil {
+			logger.Fatalw("Failed to adjust resource manager limits for the accelerated DHT client", "err", err)
+		}
+		hOpts = append(hOpts, libp2p.ResourceManager(rm))
+	}
+	h, err := libp2p.New(hOpts...)
+	if err != nil {
+		logger.Fatalw("Failed to instantiate libp2p host", "err", err)
+	}
+
+	c, err := caskadht.New(
+		caskadht.WithHost(h),
+		caskadht.WithHttpListenAddr(*httpListenAddr),
+		caskadht.WithUseAcceleratedDHT(*useAcceleratedDHT),
+	)
+	if err != nil {
+		logger.Fatalw("Failed to instantiate caskadht", "err", err)
+	}
+	ctx := context.Background()
+	if err := c.Start(ctx); err != nil {
+		logger.Fatalw("Failed to start caskadht", "err", err)
+	}
+	sch := make(chan os.Signal, 1)
+	signal.Notify(sch, os.Interrupt)
+
+	<-sch
+	logger.Info("Terminating...")
+	if err := c.Shutdown(ctx); err != nil {
+		logger.Warnw("Failure occurred while shutting down server.", "err", err)
+	} else {
+		logger.Info("Shut down server successfully.")
+	}
+}
